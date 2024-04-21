f45dad18c4a2127a0cb5668a6212f6f52e81f8e9
Arnab <arnabmondal604@gmail.com>
2024-04-21 11:47:12 +0530

    remove security file

--- blogapp/blogapp/src/main/java/com/blogapp/config/SecurityConfig.java
+++ blogapp/blogapp/src/main/java/com/blogapp/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.blogapp.config;
 
-import com.blogapp.security.CustomUserDetailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
